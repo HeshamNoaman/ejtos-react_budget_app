c0808189d5af9eb8efb957739e4d1d4458e82f7f
Hesham Mohammed Noaman <140953153+HeshamNoaman@users.noreply.github.com>
2024-04-19 16:40:04 +0300

    Budget Allocation Application lab

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    /*
+    use useState() hook to create a state variable , 
+    and inti it with current value of budget
+    */
+    const [newBudget, setNewBudget] = useState(budget);
+
+    // updates the value of newBudget
+    const handelBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+
+    return (
+        <div className="alert alert-secondary">
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handelBudgetChange} />
+        </div>
+    )
+}
+
+export default Budget;
